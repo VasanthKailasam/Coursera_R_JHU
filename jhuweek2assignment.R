e0b0fff128b53e6ec4d51e8050ba93f4e542e3af
Vasanth Kailasam <vasanthk@mail.usf.edu>
2017-01-14 17:21:56 -0500

    PolluteMean
    Finding mean of either of the two pollutants (sulfate or nitrate) across 320 monitors from 320 data files.

--- jhuweek2assignment.R
+++ jhuweek2assignment.R
@@ -4,7 +4,7 @@ pollutantmean <- function(dir, pollutant, id=1:332){
  l <- 0
  for (i in id)
   {
-    link <- sprintf("%s/%03d.csv",dir,i)
+    link <- sprintf("%s/%03d.csv",dir,i)    #dynamically changing the directory and file address to fetch different files
     dataset <- read.csv(link)
     temp <- sum(dataset[[pollutant]], na.rm=TRUE)
     if(is.na(temp))
@@ -13,7 +13,7 @@ pollutantmean <- function(dir, pollutant, id=1:332){
         temp <- 0
       }
     m <- m+ temp
-    l <- l + length(which(!is.na(dataset[pollutant])))
+    l <- l + length(which(!is.na(dataset[pollutant])))     #calculating the length of complete observations for 2 columns. 
   }
   total <- m/l
   total
